148358f2b1bd244fa61c2ed1844121fbb3c0ea79
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-17 01:26:10 +0300

    input: touchscreen: fts_spi: expose fod_status via sysfs
    Change-Id: I560b78ae0339cc41f7245d0a46908d13d7f36af3

--- drivers/input/touchscreen/fts_spi/fts.c
+++ drivers/input/touchscreen/fts_spi/fts.c
@@ -3628,6 +3628,26 @@ static ssize_t fts_double_tap_store(struct device *dev, struct device_attribute 
 }
 #endif
 
+#ifdef FTS_FOD_AREA_REPORT
+static ssize_t fts_fod_status_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct fts_ts_info *info = dev_get_drvdata(dev);
+
+	return snprintf(buf, TSP_BUF_SIZE, "%d\n", info->fod_status);
+}
+
+static ssize_t fts_fod_status_store(struct device *dev, struct device_attribute *attr,
+				    const char *buf, size_t count)
+{
+	struct fts_ts_info *info = dev_get_drvdata(dev);
+
+	sscanf(buf, "%u", &info->fod_status);
+	queue_work(info->event_wq, &info->mode_handler_work);
+
+	return count;
+}
+#endif
+
 static DEVICE_ATTR(fts_lockdown, (S_IRUGO | S_IWUSR | S_IWGRP),
 		   fts_lockdown_show, fts_lockdown_store);
 static DEVICE_ATTR(fwupdate, (S_IRUGO | S_IWUSR | S_IWGRP), fts_fwupdate_show,
@@ -3696,6 +3716,11 @@ static DEVICE_ATTR(grip_area, (S_IRUGO | S_IWUSR | S_IWGRP),
 #ifdef GESTURE_MODE
 static DEVICE_ATTR(double_tap, (S_IRUGO | S_IWUSR | S_IWGRP),
 		   fts_double_tap_show, fts_double_tap_store);
+#endif
+
+#ifdef FTS_FOD_AREA_REPORT
+static DEVICE_ATTR(fod_status, (S_IRUGO | S_IWUSR | S_IWGRP),
+		   fts_fod_status_show, fts_fod_status_store);
 #endif
 
 static DEVICE_ATTR(hover_tune, (S_IRUGO | S_IWUSR | S_IWGRP), NULL, fts_hover_autotune_store);
@@ -8871,6 +8896,10 @@ static int fts_probe(struct spi_device *client)
 	if (error) {
 		logError(1, "%s ERROR: Failed to create fod_test sysfs group!\n", tag);
 	}
+
+	error = sysfs_create_file(&info->fts_touch_dev->kobj, &dev_attr_fod_status.attr);
+	if (error)
+		logError(1, "%s ERROR: Failed to create fod_status sysfs group!\n", tag);
 #endif
 	error =
 	    sysfs_create_file(&info->fts_touch_dev->kobj,
