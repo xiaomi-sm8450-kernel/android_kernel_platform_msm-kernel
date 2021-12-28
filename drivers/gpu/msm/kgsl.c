bf2b88802d52fc70b4693763193fd684391e0430
bsp-open <bsp-open@xiaomi.com>
2021-12-28 15:22:55 +0800

    Kernel: Xiaomi kernel changes for Xiaomi 12 Pro and Xiaomi 12 Android S
    The Patch based on QualComm release TAG:LA.VENDOR.1.0.r1-09100-r1.0.r1_000011.0
    The kernel config file used are zeus_GKI.config and cupid_GKI.config
    
    Change-Id: I44f8469c8da50d238a0c0d8da2405f5b7eb66e12
    Signed-off-by: bsp-open <bsp-open@xiaomi.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2008-2021, The Linux Foundation. All rights reserved.
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 #include <uapi/linux/sched/types.h>
@@ -1115,7 +1116,7 @@ static struct kgsl_process_private *kgsl_process_private_open(
 	 * private destroy is triggered but didn't complete. Retry creating
 	 * process private after sometime to allow previous destroy to complete.
 	 */
-	for (i = 0; (PTR_ERR_OR_ZERO(private) == -EEXIST) && (i < 50); i++) {
+	for (i = 0; (PTR_ERR_OR_ZERO(private) == -EEXIST) && (i < 100); i++) {
 		usleep_range(10, 100);
 		private = _process_private_open(device);
 	}
@@ -1240,7 +1241,7 @@ static int kgsl_release(struct inode *inodep, struct file *filep)
 static int kgsl_open_device(struct kgsl_device *device)
 {
 	int result = 0;
-
+	pr_info("kgsl: kgsl_open_device Enter\n");
 	mutex_lock(&device->mutex);
 	if (device->open_count == 0) {
 		result = device->ftbl->first_open(device);
@@ -1250,6 +1251,7 @@ static int kgsl_open_device(struct kgsl_device *device)
 	device->open_count++;
 out:
 	mutex_unlock(&device->mutex);
+	pr_info("kgsl: kgsl_open_device Exit\n");
 	return result;
 }
 
