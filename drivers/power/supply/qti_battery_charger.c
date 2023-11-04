fa11942ba158cd898b510551b4f8256590e830be
Arian <arian.kulmer@web.de>
2023-11-04 11:10:48 +0100

    fixup! power: supply: qti_battery_charger: Import xiaomi changes

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -421,7 +421,7 @@ static void handle_message(struct battery_chg_dev *bcdev, void *data,
 			ack_set = true;
 			break;
 		}
-		if (validate_message(bcdev, resp_msg, len) &&
+		if (validate_message(resp_msg, len) &&
 			resp_msg->property_id < pst->prop_count) {
 			pst->prop[resp_msg->property_id] = resp_msg->value;
 			ack_set = true;
